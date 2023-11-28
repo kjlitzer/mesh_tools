99394d0984205939e79836193aab3380f10c692c
carl <kjlitzer@gmail.com>
2023-11-28 07:17:54 -0700

    first commit
    first commit

---
+++ utils/rsi_ship_scraper.py
@@ -0,0 +1,18 @@
+"""
+Scraping instructions: 
+
+https://www.reddit.com/r/starcitizen/comments/ab0gja/how_to_download_3d_print_holo_viewer_models/
+
+"""
+
+# TODO: get URL for RSI webpage
+base_url = "https://robertsspaceindustries.com/"
+
+# something like
+"https://robertsspaceindustries.com/media/21qi67il3hjrar/source/VANDUUL-Scythe4.ctm" 
+
+
+# TODO: 
+# you have to open the source code (strg + u) then searsch for "ctm" (strg + f) then you find a path in case of the hawk
+
+# TODO: convert from CTM to STL? (or OBJ?)
