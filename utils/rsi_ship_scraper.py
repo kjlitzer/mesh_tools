e7a7dc4843b302403086d6fb83fcf3713638c204
carl <kjlitzer@gmail.com>
2023-12-22 11:04:05 -0700

    buncha updates to scraping

--- utils/rsi_ship_scraper.py
+++ utils/rsi_ship_scraper.py
@@ -1,75 +1,152 @@
 """
-Scraping instructions: 
+Tool for automated scraping and mesh editing of SC holoviewer models.
 
+Manual Scraping instructions:
 https://www.reddit.com/r/starcitizen/comments/ab0gja/how_to_download_3d_print_holo_viewer_models/
-
 """
-# TODO: get a requirements.txt going
-import datetime as dt
-import os
-import tempfile
-import webbrowser
 
-from bs4 import BeautifulSoup
-import PyQt5
-import bpy
+# TODO: get a requirements.txt going
+import os
+import time
+import tempfile
+import datetime as dt
+import webbrowser
+from urllib.parse import urljoin
+
 import requests
 import html5lib
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import PyQt5
+import bpy
 
 
-# TODO: get URL for RSI webpage
-base_url = "https://robertsspaceindustries.com/"
+def scroller(url, scroll_pause_time_sec=1.0):
+    """
+    Opens URL with selenium, attempts to scroll to bottom of page, then returns soup content
 
-# something like
-"https://robertsspaceindustries.com/media/21qi67il3hjrar/source/VANDUUL-Scythe4.ctm" 
+    :param url: String with url for page to open, then scroll
+    :param scroll_pause_time_sec: time in seconds to define for time.sleep() between each scroll attempt
+    :return: beautifulsoup soup object
+    """
 
-# Start scraping
-BeautifulSoup
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
 
-pledge_ships_url = os.path.join(base_url, 'pledge/ships/')
-page = requests.get(pledge_ships_url)
+    try:
+        driver.get(url)
 
-# TODO need to find a way to close the banner page that pops up (i think cookies allows this to be suppressed
+        last_height = driver.execute_script("return document.body.scrollHeight")
 
-# with tempfile.TemporaryDirectory() as tmp_dir:
-#     tmp_fn = os.path.join(tmp_dir, 'page.html')
-#     with open(tmp_fn, 'wb') as f:
-#         f.write(page.content)
-#     webbrowser.open(tmp_fn)
-#     print()
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # TODO: change this from a crude sleep to either an implicit wait (better, but not ideal)
+            #       or and explicit wait (ideal) that specifically checks for the existence of some content
+            time.sleep(scroll_pause_time_sec)
+
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        return soup
+
+    except Exception as e:
+        print(f"Failed to scroll though web page to load additional dynamic content! Exception: {e}")
+
+    finally:
+        driver.quit()
 
 
-# soup = BeautifulSoup(page.content, "html.parser")
-soup = BeautifulSoup(page.content, "html5lib")
+def open_url_in_browser_locally(url):
+    """
+    Opens page in a web browser using selenium's web driver.
 
-print(soup.prettify())
+    :param url: string containing web URL
+    :return:
+    """
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+    driver.get(url)
 
-# Find ship links on this page, then iterate over them to get and extract data from each page
-search_results = soup.find(id='search-results')
 
-ship_items = search_results.find_all('li')
-# TODO: this list is truncated, but gets longer if you scroll, how do make this full?
+def open_page_in_browser_locally(page):
+    """
+    Opens page in a web browser, saving HTML locally first rather than requesting it from server.
 
-# TODO: use selenium to load page, use webdriver to scroll (and do anything else), then send this content to beautifulsoup
+    :param page: Requests.get() response
+    :return:
+    """
 
-raise NotImplemented()
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_fn = os.path.join(tmp_dir, 'page.html')
+        with open(tmp_fn, 'wb') as f:
+            f.write(page.content)
+        webbrowser.open(tmp_fn)
+        print()
 
-# TODO: 
-# you have to open the source code (strg + u) then searsch for "ctm" (strg + f) then you find a path in case of the hawk
 
-# TODO: convert from CTM to STL? (or OBJ?)
+def main():
 
-# TODO: store files on NAS at Z:/
-storage_path = r"Z:\User Data\3d_printing\asset_extraction\SC\holoviewer"
+    # Start scraping
+    base_url = "https://robertsspaceindustries.com/"
+    # pledge_ships_url = os.path.join(base_url, 'pledge/ships/')
+    pledge_ships_url = urljoin(base_url, 'pledge/ships/')
 
-# TODO: get blender API in here and accessible
-bpy
+    scroll = True
+    if not scroll:
+        page = requests.get(pledge_ships_url)
+        soup = BeautifulSoup(page.content, "html5lib")
+    else:
+        soup = scroller(pledge_ships_url, scroll_pause_time_sec=1.0)
 
-# TODO: turn everything into a function
+    # Find ship links on this page, then iterate over them to get and extract data from each page
+    search_results = soup.find(id='search-results')
+    ship_items = search_results.find_all('li')
+    ship_page_urls = []
 
-# TODO: build up a QT GUI for click and get type stuff
-PyQt5
+    for si in ship_items:
+        path = si.find(class_='trans-02s').find(class_='trans-02s')["href"]
+        # ship_page_url = base_url + '/' + str(path)
+        ship_page_url = urljoin(base_url, path)
+        ship_page_urls.append(ship_page_url)
 
-# TODO: date based directory for saving assets
-# TODO: temporary file storage?
-# TODO: support using pre-existing files?
+    print(f"Individual ship page URLs: ({len(ship_page_urls)})")
+    for spu in ship_page_urls:
+        print(f"{spu}")
+
+    # Go to each page and find the path to the CTM model file in the holoviewer
+    for spu in ship_page_urls:
+        print(f"Attempting to fetching model for {spu.split('/')[-1]}")
+
+        open_url_in_browser_locally(spu)
+
+        print()
+        page = requests.get(spu)
+        soup = BeautifulSoup(page.content, "html5lib")
+
+
+    raise NotImplemented()
+
+    # TODO: convert from CTM to STL? (or OBJ?)
+
+    # TODO: store files on NAS at Z:/
+    storage_path = r"Z:\User Data\3d_printing\asset_extraction\SC\holoviewer"
+
+    # TODO: get blender API in here and accessible
+    bpy
+
+    # TODO: turn everything into a function
+
+    # TODO: build up a QT GUI for click and get type stuff
+    PyQt5
+
+    # TODO: date based directory for saving assets
+    # TODO: temporary file storage?
+    # TODO: support using pre-existing files?
+
+
+if __name__ == "__main__":
+    main()
